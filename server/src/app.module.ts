850cebed26b0bce11d3d5661fe87712f5f92ce31
Florian Schaessens <f.schaessens@gmail.com>
2024-12-12 20:55:14 +0100

    config nginx

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
+import * as path from 'node:path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: path.resolve(__dirname, '../../.env')
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
